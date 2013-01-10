905f51da24a1e166886e5ead7ee45bb4c6bd8466
Dimitris Verraros <vervas.g@gmail.com>
2013-01-10 15:52:39 +0100

    Edit configuration and dependencies

--- src/main/java/com/verraros/twilike/HomeController.java
+++ src/main/java/com/verraros/twilike/HomeController.java
@@ -2,14 +2,13 @@ package com.verraros.twilike;
 
 import java.text.DateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Handles requests for the application home page.
@@ -23,17 +22,24 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
+	@ResponseBody
+	public Object home() {
+		logger.info("Welcome home!");
+		/*
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
+		String formattedDate = dateFormat.format(date);*/
 		
-		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		
+		
+		return new SomeObject();
 	}
 	
+}
+
+class SomeObject{
+	
+	String abc="defg", crate="store";
 }
